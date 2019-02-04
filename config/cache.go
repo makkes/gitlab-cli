037bb212f605cdb471b0238a560282d4c287b2eb
Max Jonas Werner <mail@makk.es>
2019-02-04 16:16:27 +0100

    Added tests for Cache

--- config/cache.go
+++ config/cache.go
@@ -2,35 +2,42 @@ package config
 
 import "encoding/json"
 
+// Cache stores values indexed by a cache name and a cache key.
 type Cache struct {
 	data map[string]map[string]string
 }
 
+// NewCache creates a new empty Cache.
 func NewCache() *Cache {
 	return &Cache{
 		data: make(map[string]map[string]string),
 	}
 }
 
-func (c *Cache) Put(k1, k2, v string) {
-	if c.data[k1] == nil {
-		c.data[k1] = make(map[string]string)
+// Put stores a value index by a cache name and a cache key.
+func (c *Cache) Put(cacheName, key, v string) {
+	if c.data[cacheName] == nil {
+		c.data[cacheName] = make(map[string]string)
 	}
-	c.data[k1][k2] = v
+	c.data[cacheName][key] = v
 }
 
-func (c *Cache) Get(k1, k2 string) string {
-	return c.data[k1][k2]
+// Get returns the value pointed to by cacheName and key.
+func (c *Cache) Get(cacheName, key string) string {
+	return c.data[cacheName][key]
 }
 
+// Flush removes all values from the cache.
 func (c *Cache) Flush() {
 	c.data = make(map[string]map[string]string)
 }
 
+// MarshalJSON makes Cache implement json.Marshaler.
 func (c *Cache) MarshalJSON() ([]byte, error) {
 	return json.Marshal(c.data)
 }
 
+// UnmarshalJSON makes Cache implement json.Unmarshaler
 func (c *Cache) UnmarshalJSON(data []byte) error {
 	aux := make(map[string]map[string]string)
 	if err := json.Unmarshal(data, &aux); err != nil {
---
+++ config/cache_test.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestGetOnEmptyCache(t *testing.T) {
+	c := NewCache()
+	v := c.Get("does not", "exist")
+	if v != "" {
+		t.Errorf("Expected empty string and got %s", v)
+	}
+}
+
+func TestPutAndGet(t *testing.T) {
+	c := NewCache()
+	c.Put("c1", "k1", "v1")
+	v := c.Get("c1", "k1")
+	if v != "v1" {
+		t.Errorf("Expected '%s' to be 'v1'", v)
+	}
+}
+func TestFlush(t *testing.T) {
+	c := NewCache()
+	c.Put("c1", "k1", "v1")
+	c.Flush()
+	if c.Get("c1", "k1") != "" {
+		t.Error("Expected cache to be empty")
+	}
+}
+func TestMarshalEmptyCache(t *testing.T) {
+	c := NewCache()
+	bytes, err := c.MarshalJSON()
+
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+	if string(bytes) != "{}" {
+		t.Errorf("Expected empty JSON object but got %s", bytes)
+	}
+}
+
+func TestMarshalPopulatedCache(t *testing.T) {
+	c := NewCache()
+	c.Put("c1", "k1", "v1")
+	c.Put("c1", "k2", "v2")
+	c.Put("c2", "k1", "v1")
+
+	bytes, err := c.MarshalJSON()
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+	if string(bytes) != `{"c1":{"k1":"v1","k2":"v2"},"c2":{"k1":"v1"}}` {
+		t.Errorf("Got unexpected JSON object: %s", bytes)
+	}
+}
+func TestUnmarshalPopulatedCache(t *testing.T) {
+	c := NewCache()
+
+	err := c.UnmarshalJSON([]byte(`{"c1":{"k1":"v1","k2":"v2"},"c2":{"k1":"v1"}}`))
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	v := c.Get("c1", "k1")
+	if v != "v1" {
+		t.Errorf("Expected '%s' to be 'v1'", v)
+	}
+	v = c.Get("c1", "k2")
+	if v != "v2" {
+		t.Errorf("Expected '%s' to be 'v2'", v)
+	}
+	v = c.Get("c2", "k1")
+	if v != "v1" {
+		t.Errorf("Expected '%s' to be 'v1'", v)
+	}
+}
+
+func TestUnmarshalBrokenJSON(t *testing.T) {
+	c := NewCache()
+	err := c.UnmarshalJSON([]byte(`broken`))
+	if err == nil {
+		t.Errorf("Expected non-nil error")
+	}
+}
