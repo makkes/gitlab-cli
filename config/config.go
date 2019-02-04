68d0e2480378a8dda019aa7aa6cc49c755498fac
Max Jonas Werner <mail@makk.es>
2019-02-04 16:42:52 +0100

    Security fix: config file permissions too broad

--- config/config.go
+++ config/config.go
@@ -17,8 +17,32 @@ type Config struct {
 
 var defaultConfig = Config{}
 
+func checkPermissions(f *os.File) {
+	fi, err := f.Stat()
+	if err != nil {
+		fmt.Printf("Error checking file permissions: %s\n", err)
+	}
+	if fi.Mode() != 0600 {
+		fmt.Printf("Correcting configuration file permissions from %#o to %#o\n", fi.Mode(), 0600)
+		err = f.Chmod(0600)
+		if err != nil {
+			fmt.Printf("Error correcting configuration file permissions: %s\n", err)
+			return
+		}
+	}
+}
+
 func Read() *Config {
-	bytes, err := ioutil.ReadFile(gitlabCLIConf())
+	f, err := os.Open(gitlabCLIConf())
+	if err != nil {
+		fmt.Printf("Error opening configuration file: %s\n", err)
+		return &defaultConfig
+	}
+	defer f.Close()
+
+	checkPermissions(f)
+
+	bytes, err := ioutil.ReadAll(f)
 	if err != nil {
 		fmt.Printf("WARNING: Error reading configuration: %s\n", err)
 		return &defaultConfig
@@ -38,12 +62,11 @@ func (c *Config) Write() {
 		fmt.Printf("Error writing configuration: %s\n", err)
 		return
 	}
-	err = ioutil.WriteFile(gitlabCLIConf(), bytes, 0644)
+	err = ioutil.WriteFile(gitlabCLIConf(), bytes, 0600)
 	if err != nil {
 		fmt.Printf("Error writing configuration: %s\n", err)
 		return
 	}
-	fmt.Printf("Configuration stored in %s\n", gitlabCLIConf())
 }
 
 func gitlabCLIConf() string {
