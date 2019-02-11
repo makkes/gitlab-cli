e76f477083bf9d085719e205278ba868b0ad3ef7
Max Jonas Werner <mail@makk.es>
2019-02-11 10:14:35 +0100

    Fix index out of range error in `login` cmd

--- cmd/login/login.go
+++ cmd/login/login.go
@@ -12,7 +12,7 @@ func NewCommand(client api.APIClient, cfg config.Config) *cobra.Command {
 	return &cobra.Command{
 		Use:   "login TOKEN [URL]",
 		Short: "Login to GitLab. If URL is omitted then https://gitlab.com is used.",
-		Args:  cobra.MaximumNArgs(2),
+		Args:  cobra.RangeArgs(1, 2),
 		Run: func(cmd *cobra.Command, args []string) {
 			url := "https://gitlab.com"
 			if len(args) == 2 {
