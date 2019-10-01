02d802dc3b83f22d50b36d6c8b404193d6b580f5
Max Jonas Werner <makkes@users.noreply.github.com>
2019-10-01 16:59:13 +0200

    Improve help output of 'var create'. (#22)
    Resolves #20

--- cmd/variable/create/create.go
+++ cmd/variable/create/create.go
@@ -14,6 +14,7 @@ func NewCommand(client api.Client) *cobra.Command {
 	return &cobra.Command{
 		Use:   "create PROJECT KEY VALUE",
 		Short: "Create a project-level variable",
+		Long:  "Create a project-level variable. The KEY may only contain the characters A-Z, a-z, 0-9, and _ and must be no longer than 255 characters.",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			project, err := client.FindProject(args[0])
