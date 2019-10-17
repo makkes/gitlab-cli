5e046f5745892a922b8a4a0800b08a5971158b89
Erik Nelson <erik.david.nelson@gmail.com>
2019-10-17 13:20:39 -0400

    Add optional environment_scope arg to var create

--- cmd/variable/create/create.go
+++ cmd/variable/create/create.go
@@ -12,17 +12,22 @@ import (
 
 func NewCommand(client api.Client) *cobra.Command {
 	return &cobra.Command{
-		Use:   "create PROJECT KEY VALUE",
+		Use:   "create PROJECT KEY VALUE [ENVIRONMENT_SCOPE]",
 		Short: "Create a project-level variable",
 		Long:  "Create a project-level variable. The KEY may only contain the characters A-Z, a-z, 0-9, and _ and must be no longer than 255 characters.",
-		Args:  cobra.ExactArgs(3),
+		Args:  cobra.RangeArgs(3, 4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			project, err := client.FindProject(args[0])
 			if err != nil {
 				return fmt.Errorf("Cannot create variable: %s", err)
 			}
+
+			body := fmt.Sprintf("key=%s&value=%s", url.QueryEscape(args[1]), url.QueryEscape(args[2]))
+			if len(args) == 4 {
+				body += fmt.Sprintf("&environment_scope=%s", url.QueryEscape(args[3]))
+			}
 			_, _, err = client.Post("/projects/"+strconv.Itoa(project.ID)+"/variables",
-				strings.NewReader(fmt.Sprintf("key=%s&value=%s", url.QueryEscape(args[1]), url.QueryEscape(args[2]))))
+				strings.NewReader(body))
 			if err != nil {
 				return fmt.Errorf("Cannot create variable: %s", err)
 			}
