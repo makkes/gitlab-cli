2d1a0592334d013025ce79cb0527f5edcb3f4b8a
Max Jonas Werner <mail@makk.es>
2020-01-03 19:29:54 +0100

    Improved install script
    * Make it robust against partial downloads: The invocation of
      'do_install' at the very end of the script ensures that it has been
      downloaded completely prior to running the installation steps.
    * Added more verbose output

--- install.sh
+++ install.sh
@@ -29,49 +29,53 @@ check_prereqs() {
     fi
 }
 
-check_prereqs
+do_install() {
+    target_dir="/usr/local/bin"
+    while getopts "b:" arg; do
+        case "$arg" in
+          b)
+              target_dir="$OPTARG"
+              ;;
+          *)
+              exit 1
+              ;;
+        esac
+    done
+    shift $((OPTIND - 1))
 
-target_dir="/usr/local/bin"
-while getopts "b:" arg; do
-    case "$arg" in
-      b)
-          target_dir="$OPTARG"
-          ;;
-      *)
-          exit 1
-          ;;
+    version=${1:-}
+    [ -z "$version" ] && version="latest"
+
+    set +e
+    release_json=$(curl -fsLH 'Accept: application/json' https://github.com/makkes/gitlab-cli/releases/${version})
+    set -e
+    [ -z "$release_json" ] && echoerr "Unknown version ${version}" && exit 1
+    release_tag=$(echo "$release_json"|grep -o '"tag_name":"[^"]*"'|cut -d":" -f2|sed s/\"//g)
+
+    kernel_name=$(uname -s)
+    machine=$(uname -m)
+    case "${kernel_name}" in
+        Darwin)
+            os="darwin"
+            ;;
+        Linux)
+            os="linux"
+            ;;
+        *)
+            echoerr "Unsupported OS ${kernel_name}" && exit 1
+            ;;
     esac
-done
-shift $((OPTIND - 1))
+    [ "${machine}" != "x86_64" ] && echoerr "Unsupported CPU architecture ${machine}" && exit 1
+    arch="amd64"
 
-version=${1:-}
-[ -z "$version" ] && version="latest"
+    download_url="https://github.com/makkes/gitlab-cli/releases/download/${release_tag}/gitlab_${release_tag}_${os}_${arch}"
+    tmpdir=$(mktemp -d)
+    echo "Downloading gitlab ${release_tag}..."
+    curl --progress-bar -Lo "${tmpdir}"/gitlab "${download_url}"
+    install -t "${target_dir}" "${tmpdir}/gitlab"
 
-set +e
-release_json=$(curl -fsLH 'Accept: application/json' https://github.com/makkes/gitlab-cli/releases/${version})
-set -e
-[ -z "$release_json" ] && echoerr "Unknown version ${version}" && exit 1
-release_tag=$(echo "$release_json"|grep -o '"tag_name":"[^"]*"'|cut -d":" -f2|sed s/\"//g)
+    echo "Installed gitlab ${release_tag} into ${target_dir}"
+}
 
-kernel_name=$(uname -s)
-machine=$(uname -m)
-case "${kernel_name}" in
-    Darwin)
-        os="darwin"
-        ;;
-    Linux)
-        os="linux"
-        ;;
-    *)
-        echoerr "Unsupported OS ${kernel_name}" && exit 1
-        ;;
-esac
-[ "${machine}" != "x86_64" ] && echoerr "Unsupported CPU architecture ${machine}" && exit 1
-arch="amd64"
-
-download_url="https://github.com/makkes/gitlab-cli/releases/download/${release_tag}/gitlab_${release_tag}_${os}_${arch}"
-tmpdir=$(mktemp -d)
-curl -Lso "${tmpdir}"/gitlab "${download_url}"
-install -t "${target_dir}" "${tmpdir}/gitlab"
-
-echo "Installed gitlab ${release_tag} into ${target_dir}"
+check_prereqs
+do_install
