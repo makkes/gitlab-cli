7cb6488ea2162d121c72637c0dd89f55ca9e8af1
Max Jonas Werner <mail@makk.es>
2023-04-05 16:12:22 +0200

    build: fix update command
    After moving to Goreleaser in 3947282bc090c5d92c505fde150196013280aa09
    the artifacts are put into a gzipped tar archive.

--- cmd/update/update.go
+++ cmd/update/update.go
@@ -1,6 +1,8 @@
 package update
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"net/http"
@@ -36,16 +38,17 @@ func updateCommand(dryRun bool, includePreReleases bool, upgradeMajor bool, out 
 				latestVersionString, repo+"/releases/"+latestVersionString)
 			return nil
 		}
-		downloadURL := fmt.Sprintf("%s/releases/download/%s/gitlab_%s_%s_%s",
+		downloadURL := fmt.Sprintf("%s/releases/download/%s/gitlab_%s_%s_%s.tar.gz",
 			repo, latestVersionString, latestVersionString, runtime.GOOS, runtime.GOARCH)
 		fmt.Fprintf(out, "Updating to %s\n", latestVersionString)
 		resp, err := http.Get(downloadURL) // #nosec G107
 		if err != nil {
-			return fmt.Errorf("could not download latest release: %w", err)
+			return fmt.Errorf("could not download latest release from %s: %w", downloadURL, err)
 		}
 		if resp.StatusCode != http.StatusOK {
-			return fmt.Errorf("could not download latest release: received HTTP status %d", resp.StatusCode)
+			return fmt.Errorf("could not download latest release from %s: received HTTP status %d", downloadURL, resp.StatusCode)
 		}
+
 		exec, err := getExecutable()
 		if err != nil {
 			return fmt.Errorf("could not get current executable to update: %w", err)
@@ -59,7 +62,22 @@ func updateCommand(dryRun bool, includePreReleases bool, upgradeMajor bool, out 
 		if err != nil {
 			return fmt.Errorf("could not open binary for updating: %w", err)
 		}
-		_, err = io.Copy(binary, resp.Body)
+		defer func() {
+			os.Remove(dest)
+		}()
+
+		gzReader, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed creating gzip reader: %w", err)
+		}
+		defer gzReader.Close()
+
+		tarReader := tar.NewReader(gzReader)
+		if _, err := tarReader.Next(); err != nil {
+			return fmt.Errorf("failed advancing in tar archive: %w", err)
+		}
+
+		_, err = io.Copy(binary, tarReader)
 		binary.Close()
 		if err != nil {
 			return fmt.Errorf("could not download new version: %w", err)
